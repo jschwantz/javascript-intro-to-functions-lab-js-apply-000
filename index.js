98d116e866f9bca2b7d4072d9bd88378124c8222
Jared Schwantz <c.jared.schwantz@gmail.com>
2019-05-21 02:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,13 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if string === 'I love you, Grandma.' 
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  else {
+    return "I can't hear you!"
+  }
 }
